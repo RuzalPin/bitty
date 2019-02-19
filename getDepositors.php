a9461fe5fe0350fba21e55ef3b7c0cc0018edaf6
ruzal <ruzal.gabdrahmanoff@yandex.ru>
2019-02-19 23:27:26 +0300

    change content

--- getDepositors.php
+++ getDepositors.php
@@ -4,22 +4,37 @@
 	file_put_contents("Depositors.txt",""); /* Clear file */
 	$current = file_get_contents($file);
 
-	$mysqli = new mysqli("localhost","root","root");
+	$mysqli = new mysqli("127.0.0.1","hz","anythingelse?");
 
 		if ($mysqli->connect_errno) {
 			printf("Ошибка подключения к бд", $mysqli->connect_error);
 			exit();
 		}
 
-		if ($result = $mysqli->query("SELECT * FROM pm_common.players ")) {
-			printf("Select вернул \n", $result->num_rows);
-		
+		if ($result = $mysqli->query("SELECT `players`.`login` 
+FROM `table`.`deposit` dep
+INNER JOIN `table`.`players` players ON `players`.`login` = dep.login
+WHERE face = 'ha-ha-ha' AND blocked = 0 
+GROUP BY dep.`login`; ")) {
+
+			
+
+			while($rows = $result->fetch_assoc()) {
+	
+				printf($rows["login"]);
+				printf("\n");
+				$current .= "\"".$rows["login"]."\"".",";
+
+			}
+
+			/* get and delete last , */
+			$current = rtrim($current,","); 
+			
+
 			$result->close();
 		}
 
 	$mysqli->close();
-
-	$current .= "\"Yo-ho-ho\",";
 
 
 	file_put_contents($file,$current);
